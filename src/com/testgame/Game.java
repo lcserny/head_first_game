b548fb83189e13350b765d3f5231fd626da9ea91
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-01-18 14:38:58 +0200

    MAIN adds Game class

---
+++ src/com/testgame/Game.java
@@ -0,0 +1,18 @@
+package com.testgame;
+
+import javax.swing.JFrame;
+import java.awt.*;
+
+/**
+ * Created by user on 18.01.2016.
+ */
+public class Game
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Plain title");
+        frame.setSize(300,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
