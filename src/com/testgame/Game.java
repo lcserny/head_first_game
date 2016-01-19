0bf7a7a19f34a82b708dd9472d292cc935e43501
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-01-19 13:47:04 +0200

    MAIN adds ball border bounce

--- src/com/testgame/Game.java
+++ src/com/testgame/Game.java
@@ -14,25 +14,57 @@ import java.awt.geom.Ellipse2D;
 @SuppressWarnings("serial")
 public class Game extends JPanel
 {
+    int x = 0;
+    int y = 0;
+    int xa = 1;
+    int ya = 1;
+
+    private void moveBall()
+    {
+        if (x + xa < 0) {
+            xa = 1;
+        }
+
+        if (x + xa > getWidth() - 30) {
+            xa = -1;
+        }
+
+        if (y + ya < 0) {
+            ya = 1;
+        }
+
+        if (y + ya > getHeight() - 30) {
+            ya = -1;
+        }
+
+        x = x + xa;
+        y = y + ya;
+    }
+
     @Override
     public void paint(Graphics g)
     {
+        super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.RED);
-        g2d.fillOval(0, 0, 30, 30);
-        g2d.drawOval(0, 50, 30, 30);
-        g2d.fillRect(50, 0, 30, 30);
-        g2d.drawRect(50, 50, 30, 30);
-
-        g2d.draw(new Ellipse2D.Double(0, 100, 30, 30));
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.fillOval(x, y, 30, 30);
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
-        JFrame frame = new JFrame("Plain title");
-        frame.add(new Game());
-        frame.setSize(300,300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = new JFrame("Mini Tennis");
+        Game game = new Game();
+
+        frame.add(game);
+        frame.setSize(300,400);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while (true) {
+            game.moveBall();
+            game.repaint();
+
+            Thread.sleep(10);
+        }
     }
 }
