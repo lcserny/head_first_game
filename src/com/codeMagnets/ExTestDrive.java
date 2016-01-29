4be68642ea28ead40c34fd86f635f099e8072430
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-01-29 13:07:15 +0200

    CH11 adds ExTestDrive codemagnet

---
+++ src/com/codeMagnets/ExTestDrive.java
@@ -0,0 +1,34 @@
+package com.codeMagnets;
+
+/**
+ * Created by user on 29.01.2016.
+ */
+public class ExTestDrive
+{
+    static void doRisky(String t) throws MyEx
+    {
+        System.out.print("h");
+
+        if ("yes".equals(t)) {
+            throw new MyEx();
+        }
+
+        System.out.print("r");
+        System.out.print("o");
+    }
+
+    public static void main (String[] args)
+    {
+        String test = args[0];
+        System.out.print("t");
+
+        try {
+            doRisky(test);
+        } catch (MyEx e) {
+            System.out.print("a");
+        } finally {
+            System.out.print("w");
+            System.out.print("s");
+        }
+    }
+}
---
+++ src/com/codeMagnets/MyEx.java
@@ -0,0 +1,9 @@
+package com.codeMagnets;
+
+/**
+ * Created by user on 29.01.2016.
+ */
+public class MyEx extends Exception
+{
+
+}
