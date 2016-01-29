43f23bacbf32105ab1081bc3ba80d756eac37e9b
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-01-29 14:08:45 +0200

    CH12 adds simple GUI button handling

--- src/com/guis/SimpleGuiOne.java
+++ src/com/guis/SimpleGuiOne.java
@@ -1,20 +1,40 @@
 package com.guis;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 /**
  * Created by user on 29.01.2016.
  */
-public class SimpleGuiOne
+public class SimpleGuiOne implements ActionListener
 {
+    JButton button;
+    static final String defaultButtonText = "click me";
+
     public static void main(String[] args)
     {
+        SimpleGuiOne guiOne = new SimpleGuiOne();
+        guiOne.go();
+    }
+
+    public void go()
+    {
         JFrame frame = new JFrame();
-        JButton button = new JButton("click me");
+        button = new JButton(defaultButtonText);
+        button.addActionListener(this);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(button);
         frame.setSize(300, 300);
         frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent event)
+    {
+        if (button.getText().equals(defaultButtonText)) {
+            button.setText("I've been clicked");
+        } else {
+            button.setText(defaultButtonText);
+        }
     }
 }
