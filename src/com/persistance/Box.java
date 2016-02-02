f56e165058f5b0f14fe5286f66fb893de10fbf15
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-02-02 13:07:30 +0200

    CH14 adds persistence Box test

---
+++ src/com/persistance/Box.java
@@ -0,0 +1,38 @@
+package com.persistance;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * Created by user on 02.02.2016.
+ */
+public class Box implements Serializable
+{
+    private int width;
+    private int height;
+
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+
+    public static void main(String[] args)
+    {
+        Box myBox = new Box();
+        myBox.setWidth(50);
+        myBox.setHeight(20);
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream("data");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(myBox);
+            objectOutputStream.close();
+        } catch (Exception ex) { ex.printStackTrace(); }
+    }
+}
