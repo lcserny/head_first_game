1b56aedcd07e1a799ad22d5ce881aba45667fe52
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-02-08 14:13:33 +0200

    CH16 adds songlist text and jukebox1

---
+++ src/com/sorting/JukeBox1.java
@@ -0,0 +1,43 @@
+package com.sorting;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+/**
+ * Created by user on 08.02.2016.
+ */
+public class JukeBox1
+{
+    ArrayList<String> songList = new ArrayList<String>();
+
+    public static void main(String[] args)
+    {
+        new JukeBox1().go();
+    }
+
+    public void go()
+    {
+        getSongs();
+        System.out.println(songList);
+    }
+
+    void getSongs()
+    {
+        try {
+            File file = new File("SongList.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        } catch (Exception ex) { ex.printStackTrace(); }
+    }
+
+    void addSong(String lineToParse)
+    {
+        String[] tokens = lineToParse.split("/");
+        songList.add(tokens[0]);
+    }
+}
